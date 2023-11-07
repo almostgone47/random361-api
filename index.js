4b8294e29295260616913137811cf911cc04d1af
almostgone47 <almostgone47@hotmail.com>
2023-11-07 20:27:40 +0530

    remove header requirement

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ app.use(
   }),
 );
 
-// app.use(function (req, res, next) {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET');
-//   res.setHeader('Access-Control-Allow-Headers:', '*');
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  // res.setHeader('Access-Control-Allow-Headers:', '*');
+  next();
+});
 
 const locations = [
   'Boston',
